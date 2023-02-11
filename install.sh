f2b938dd16a2afbfd17811df38abf1330e02523a
Alberto Gucciardi <retablo@retablo.eu>
2023-02-11 21:50:20 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# checking the existence of apache2
-
-if ![-d '/etc/apache2' ];then
-    sudo apt install apache2 -y
-end
+# checking the existence of /var/www/mydesk.agconsulente.company
+if ![[-d /var/www/mydesk.agconsulente.company]];then
+    echo 'Folder does not exist, hold on please while we\'re creating 
+    sudo mkdir -p /var/www/mydesk.agconsulente.company
+fi
 
 # check if not exists 
